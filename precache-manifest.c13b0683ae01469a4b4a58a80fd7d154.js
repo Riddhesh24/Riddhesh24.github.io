6745b2bc8b9c19d4f3bc960b772b0af8749a7da1
Riddhesh Sawant <riddheshsawant@Riddheshs-MacBook-Pro.local>
2025-02-12 20:01:41 -0800

    Updates

---
+++ precache-manifest.c13b0683ae01469a4b4a58a80fd7d154.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029e77b41ddc024aabe4133f46dbecca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa489d235f386fbfad5f",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "3aa73e98ae637711d531",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "f1e9de51f2554392e6d8",
+    "url": "/static/css/main.7c74993e.chunk.css"
+  },
+  {
+    "revision": "99279d053dc70f0d47d1",
+    "url": "/static/js/2.cd7b7405.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/static/js/2.cd7b7405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa489d235f386fbfad5f",
+    "url": "/static/js/3.00e37152.chunk.js"
+  },
+  {
+    "revision": "3aa73e98ae637711d531",
+    "url": "/static/js/4.cded0791.chunk.js"
+  },
+  {
+    "revision": "f1e9de51f2554392e6d8",
+    "url": "/static/js/main.cf507a34.chunk.js"
+  },
+  {
+    "revision": "34e6a1170ea4e4e72964",
+    "url": "/static/js/runtime-main.74318ddc.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8c0f44ace59efa01eebd66963206c0b1",
+    "url": "/static/media/bitsLogo.8c0f44ac.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "933934a498a21b94cf7944614572bdd1",
+    "url": "/static/media/credLogo.933934a4.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "19ccf4168e434911575422efc3fc3232",
+    "url": "/static/media/niyoLogo.19ccf416.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
